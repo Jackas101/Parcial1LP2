c3b2830d57d1cff65cbd243ea7d0a6a2d5e84833
Jack Ccopa <alexcco2000@gmail.com>
2020-07-16 12:08:08 -0500

    Configurar la bd

--- src/java/bd/ConectaBd.java
+++ src/java/bd/ConectaBd.java
@@ -18,8 +18,8 @@ public class ConectaBd {
         }
         return cn;
     }
-    private final String url = "jdbc:mysql://localhost:3306/abcdd";
+    private final String url = "jdbc:mysql://localhost:3306/bdbiblioteca";
     private final String driver = "com.mysql.jdbc.Driver";
     private final String user = "root";
-    private final String password = "";
+    private final String password = "12345678";
 } 
